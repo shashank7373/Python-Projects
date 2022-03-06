a100f0944d84341283cf49a8bf37254ec6f70592
shashank7373 <shashudex@gmail.com>
2022-03-06 13:08:02 +0530

    [CLI] files finalized

--- CLI_based_chat_app/client.py
+++ CLI_based_chat_app/client.py
@@ -1,10 +1,34 @@
 import socket
+import sys
+import traceback
+import threading
 
-client = socket.socket()
+c_lock = threading.Lock()
 
-client.connect(("localhost", 9999))
 
-name = input("Enter your name: ")
-client.send(bytes(name, "utf-8"))
+def main():
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = "localhost"
+    port = 9999
+    try:
+        soc.connect((host, port))
+    except Exception:
+        print("Client failed to connect to server.")
+        traceback.print_exc()
+        sys.exit(0)
+    print("Please enter quit to exit")
+    while True:
+        message = input(" -> ")
+        if message == "quit":
+            soc.send(b'--quit--')
+            break
+        soc.send(message.encode("utf8"))
+        c_lock.acquire()
+        message_received = soc.recv(5120).decode("utf8")
+        if message_received:
+            print("Message received from server: {}".format(message_received))
+            c_lock.release()
+        
 
-print(client.recv(1024).decode())
+if __name__ == "__main__":
+    main()
--- CLI_based_chat_app/server.py
+++ CLI_based_chat_app/server.py
@@ -1,21 +1,73 @@
 import socket
+import sys
+import threading
+import traceback
 
-serv = socket.socket()
-print("Server created successfully")
+s_lock = threading.Lock()
 
-serv.bind(("localhost", 9999))
 
-serv.listen(3)
-print("waiting for connections..")
+def server_socket():
+    host = "localhost"
+    port = 9999
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print("Socket created")
+    try:
+        soc.bind((host, port))
+    except Exception as msg:
+        print("Counld not bind to host {}, port {} : {}".format(host, port, msg))
+        sys.exit(0)
+    soc.listen(6)
+    print("Server is now listening..")
+    try:
+        while True:
+            (conn, ip) = soc.accept()
+            c_host, c_port = str(ip[0]), str(ip[1])
+            print("Connected with client {} : {}".format(c_host, c_port))
+            try:
+                client_thread = threading.Thread(target=client_process, args=(conn, c_host, c_port, ), )
+                client_thread.start()
+            except Exception:
+                print("Failed to create a client thread")
+                traceback.print_exc()
+            else:
+                client_thread.join()
+    except KeyboardInterrupt:
+        print("Server closed manually. Bye..")
+        soc.close()
 
-while True:
-    (client, ip_addr) = serv.accept()
-    
-    name = client.recv(1024).decode()
-    
-    print("Successfully connected with {} at (IP -> {}, Port -> {})".format(
-        name, ip_addr[0], ip_addr[1]))
-    
-    client.send(bytes("Welcome to CLI chat tool", "utf-8"))
-    
-    client.close()
+
+def client_process(connection, host, port):
+    is_active = True
+    while is_active:
+        client_input = receive_input(connection, 5120)
+        if "QUIT" in client_input:
+            print("Client is requesting to exit.")
+            connection.close()
+            print("Connect {} : {} closed".format(host, port))
+            is_active = False
+        else:
+            #s_lock.acquire()
+            processed_input = process_input(client_input)
+            print("Process result : {}".format(processed_input))
+            connection.sendall(str(processed_input)[::-1].encode("utf8"))
+            #s_lock.release()
+
+
+def receive_input(connection, max_buffer_size):
+    client_input = connection.recv(max_buffer_size)
+    client_input_size = sys.getsizeof(client_input)
+    if client_input_size > max_buffer_size:
+        print("The input size {} is greater than {}".format(client_input_size, max_buffer_size))
+    decoded_input = client_input.decode("utf8")
+    result = process_input(decoded_input)
+    return result
+
+
+def process_input(decoded_input):
+    print("Processing the input received from client : {}".format(decoded_input))
+    return "Processed input : {}".format(decoded_input.upper())
+
+
+if __name__ == "__main__":
+    server_socket()
